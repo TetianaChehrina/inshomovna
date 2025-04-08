386aa9dd23c42ad6f37269b4b11885cbd6ea2bcc
TetianaChehrina <3909139@gmail.com>
2025-04-08 16:47:20 +0200

    init project

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <>
+      <main>
+        <p>AboutPage</p>
+      </main>
+    </>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import css from "./page.module.css";
+export default function Home() {
+  return (
+    <>
+      <main className={css.page_Home}>
+        <p>HomePage</p>
+      </main>
+    </>
+  );
+}
